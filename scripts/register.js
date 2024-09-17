d88ddcd5ae627c33f4a2bd7806bde44232ec5a42
Michael Agyemang Prempeh <agyemangmichael555@gmail.com>
2024-09-17 13:50:34 +0000

    tuesday - second push

--- scripts/register.js
+++ scripts/register.js
@@ -1,3 +1,6 @@
+/*
+// THIS IS WHAT WE DID ON DAY 1
+
 // below is our first js code to test whether js is linked appropriately and is working well as well
 
 //alert("Welcome to the Welcome page!")
@@ -13,3 +16,40 @@ registerForm.onsubmit = function() {
     // save user info...
     // send configuration email...
 }
+
+*/
+
+// THIS IS WHAT WE DID ON DAY 2
+
+// select form by id
+const registerForm = document.querySelector('#register-form');
+console.log(registerForm);
+const h1 = document.createElement('h1');
+h1.innerText = "I will not mess up!";
+// document.body.appendChild(h1);
+// document.body.innerHTML = "<h1> Don't try this @Home!";
+// handle form submit event
+registerForm.addEventListener('Submit',function() {
+    // collect username
+    // collect email
+    // collect password
+    // save user info
+    // send configuration email...
+})
+
+// declare age variable
+const userAge = 44;
+
+// square of age...
+
+function squareAge(userAge) {
+    const squaredAge = userAge ** 2;
+    return squaredAge;
+}
+
+
+console.log(squareAge(userAge));
+
+console.log('');
+const squaredAge = squareAge(45);
+console.log(squaredAge);
